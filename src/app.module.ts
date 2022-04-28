884761c953dba28df04e3564933220522a97d094
tpaphysics <physics.thiago@gmail.com>
2022-04-28 01:04:26 -0300

    feat: instalação das dependencias  @nestjs/graphql @nestjs/apollo graphql apollo-server-express e configuração do app.module para configurar o apollo server graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { UsersModule } from './users/users.module';
 
 @Module({
+  imports: [
+    UsersModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [],
   providers: [],
-  imports: [UsersModule],
 })
 export class AppModule {}
