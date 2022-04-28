76f585df041255bae2069e7f89fca845422f64fe
tpaphysics <physics.thiago@gmail.com>
2022-04-28 00:59:03 -0300

    feat: iniciação do prisma com pull do banco de dados. Criação do prisma service e modulo. configuração de variáveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
+  imports: [UsersModule],
 })
 export class AppModule {}
