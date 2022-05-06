00c0ce9a9b8a08f8fc70dc6cf028c19c348b958b
tpaphysics <physics.thiago@gmail.com>
2022-05-06 12:18:50 -0300

    feat: criação do jwt-strategy.ts

---
+++ src/auth/strategies/jwt-strategy.ts
@@ -0,0 +1,25 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PayloadJwt } from '../dto/payload-jwt';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UsersService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: PayloadJwt) {
+    const { sub } = payload;
+    const user = await this.userService.findOne(sub);
+    if (!user) {
+      throw new UnauthorizedException('invalid token');
+    }
+    return user;
+  }
+}
